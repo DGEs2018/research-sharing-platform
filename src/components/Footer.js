37bcdddf1a3d7f630afbf02f49faef4b096256f0
Dawit <dawit@ginetta.net>
2019-09-13 16:52:55 +0200

    Fill in every component with the bare minimum skeletal content, along with some basic styles

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+	const footerStyle = {};
+	return (
+		<footer>
+			<p>Posted by: Poco students Basel Batch</p>
+			<p>
+				Contact information: <a href="mailto: them@example.com" />
+			</p>
+			<p>NB: this is a common share teaching resources and may not be reproduced for commercial purposes</p>
+		</footer>
+	);
+};
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Main = () => {
+	const style = {
+		display: 'inline-block',
+		height: '800px',
+		width: '500px',
+		padding: '16px',
+		margin: '16px',
+		border: '1px solid black'
+	};
+	const styleUl = {
+		listStyle: 'none',
+		textAlign: 'left',
+		fontSize: '18px',
+		fontWeight: '600',
+		lineHeight: '36px'
+	};
+	return (
+		<div style={style}>
+			<ul style={styleUl}>
+				<li>React with Wes Bos</li>
+				<li>React with Mosh Mohamodani</li>
+				<li>React Tyler McGinnis</li>
+				<li>Javascript</li>
+				<li>Master Git</li>
+				<li>Advanced CSS</li>
+				<li>Learning Sass</li>
+				<li>Understanding Vue</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Main;

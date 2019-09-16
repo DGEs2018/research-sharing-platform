2dad3b8f6da8b5dd8ca432a4b9d6f6c1ecfde5e6
Dawit <dawit@ginetta.net>
2019-09-16 12:16:53 +0200

    Import all the previously created components to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import NavigationBar from './components/NavigationBar';
+import CourseMaterialList from './components/CourseMaterialList';
+import CourseMaterial from './components/CourseMaterial';
+import CourseDetails from './components/CourseDetails';
 import './App.css';
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
 			<Layout />
+			<NavigationBar />
+			<CourseMaterialList />
+			<CourseMaterial />
+			<CourseDetails />
 		</div>
 	);
-}
+};
 
 export default App;

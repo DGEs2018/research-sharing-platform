c816af973b6941e25b7338275d3725585aaff7a3
Dawit <dawit@ginetta.net>
2019-09-16 15:32:28 +0200

    Import the newly created components to the App.js file

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import NavigationBar from './components/NavigationBar';
 import CourseMaterialList from './components/CourseMaterialList';
 import CourseMaterial from './components/CourseMaterial';
 import CourseDetails from './components/CourseDetails';
+import SearchBox from './components/SearchBox';
+import Title from './components/Title';
 import './App.css';
+import Description from './components/Description';
+import Avatar from '../../tweeter/src/components/Avatar';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Layout />
 			<NavigationBar />
+			<SearchBox />
 			<CourseMaterialList />
 			<CourseMaterial />
 			<CourseDetails />
+			<div>
+				<Avatar />
+				<Title />
+				<Description />
+			</div>
 		</div>
 	);
 };

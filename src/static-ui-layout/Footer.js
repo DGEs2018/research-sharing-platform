bfa1196d823a35b0dfb2f1f4217e7eec928ed028
Dawit <dawit@ginetta.net>
2019-09-17 13:15:17 +0200

    Edit the content of the Footer.js

--- src/static-ui-layout/Footer.js
+++ src/static-ui-layout/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
-	const footerStyle = {};
+	const footerStyle = {
+		textTransform: 'text-uppercase'
+	};
 	return (
 		<footer>
+			<h5 style={footerStyle}>Footer Content</h5>
 			<p>Posted by: Poco students Basel Batch</p>
 			<p>
-				Contact information: <a href="mailto: them@example.com" />
+				Contact information: <a href="mailto: somewhere@example.com" />
 			</p>
 			<p>NB: this is a common share teaching resources and may not be reproduced for commercial purposes</p>
+			<p>&pcopy; 2019: ourpage.ch </p>
 		</footer>
 	);
 };
